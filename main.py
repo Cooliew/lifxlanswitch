9717808ff4b4608600b2de9e98325c81ded770be
Cooliew <cooliew101@gmail.com>
2023-09-09 22:05:06 +1000

    Smoothed Lighting, Added Mode Changes for button

--- main.py
+++ main.py
@@ -3,42 +3,84 @@
 
 from lifxlan import LifxLAN
 from gpiozero import RotaryEncoder, Button
+from signal import pause
 
-rotor = RotaryEncoder(17, 18, wrap=False, max_steps=30)
+rotor = RotaryEncoder(17, 18, wrap=True, max_steps=0)
+button = Button(27)
 lifx = LifxLAN()
+light = None
+colour = None
+prevSteps = 0
 devices = lifx.get_lights()
 for device in devices:
 	print(device.get_label())
 	if device.get_label() == "Terrarium Lamp":
 		light = device
-else:
-	print('List finished')
-	print(light.get_label(), ' selected.')
-colour = list(light.get_color())
-currentSteps = int((((colour[2] / 65535) * 2) - 1) * 30)
-rotor.steps = currentSteps
+print('List finished')
+print(light.get_label(), 'selected.')
+
 
 def main():
-	while True:
-		rotor.wait_for_rotate()
-		rotor.when_rotated = dim(2000, light)
+	controller()
 
-def changeColour():
-	colour[2] = int(((rotor.value+1)/2)*65535)
+def controller():
+	button.when_pressed = switchMode
+	rotor.when_rotated = setBrightness
+	pause()
+
+def test():
 	print(rotor.steps)
-	currentSteps = rotor.steps
-	light.set_color(colour, 0, True)
 
-def dim(amount, light):
-	colour = list(light.get_color())
-	brightness = colour[2]
-	brightness += amount
-	if brightness < 0:
-		brightness = 0
-	elif brightness > 65535:
-		brightness = 65535
-	colour[2] = brightness
-	light.set_color(colour, 0, True)
+dimMode = True
+def switchMode():
+	global dimMode
+	dimMode = not dimMode
+	if dimMode:
+		rotor.when_rotated = setBrightness
+		print("Switching to brightness...")
+	else:
+		rotor.when_rotated = setColour
+		print("Switching to hue...")
+
+def setBrightness():
+	global prevSteps, colour
+	steps = rotor.steps - prevSteps
+	prevSteps = rotor.steps
+	try:
+		amount = 6000 * steps
+		if colour is None:
+			colour = list(light.get_color())
+		brightness = colour[2]
+		brightness += amount
+		if brightness < 0:
+			brightness = 0
+		elif brightness > 65535:
+			brightness = 65535
+		colour[2] = brightness
+		print(str(round((brightness / 65535) * 100)) + "%")
+		light.set_color(colour, 500, True)
+	except Exception as error:
+		print("An exception occurred:", error)
+
+def setColour():
+	global prevSteps, colour
+	steps = rotor.steps - prevSteps
+	prevSteps = rotor.steps
+	try:
+		amount = 2000 * steps
+		if colour is None:
+			colour = list(light.get_color())
+		hue = colour[0]
+		hue += amount
+		if hue < 0:
+			hue += 65535
+		elif hue > 65535:
+			hue -= 65535
+		colour[0] = hue
+		print(str(round((hue / 65535) * 360)) + "°")
+		light.set_color(colour, 500, True)
+	except Exception as error:
+		print("An exception occurred:", error)
 
 if __name__ == "__main__":
 	main()
