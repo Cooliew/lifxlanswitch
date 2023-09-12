cc8949c1bc162ccce9c7f9575e2cc72a3076ef9f
Cooliew <cooliew101@gmail.com>
2023-09-12 17:54:58 +1000

    Added display interface init code

--- main.py
+++ main.py
@@ -3,34 +3,37 @@
 
 from lifxlan import LifxLAN
 from gpiozero import RotaryEncoder, Button
-from signal import pause
 from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.oled.device import ssd1351
+from signal import pause
+from time import sleep
 
 serial = spi(device=0, port=0)
-device = ssd1351(serial)
+displayDevice = ssd1351(serial)
 
 rotor = RotaryEncoder(17, 18, wrap=True, max_steps=0)
 button = Button(27)
 lifx = LifxLAN()
-light = None
+lightDevice = None
 colour = None
 prevSteps = 0
 devices = lifx.get_lights()
 for device in devices:
 	print(device.get_label())
 	if device.get_label() == "Terrarium Lamp":
-		light = device
-print('List finished')
-print(light.get_label(), 'selected.')
+		lightDevice = device
+
+with canvas(displayDevice) as draw:
+	draw.rectangle(displayDevice.bounding_box, outline="white", fill="black")
+	draw.text((30, 40), "List finished", fill="white")
+	draw.text((30, 60), lightDevice.get_label() + "selected.", fill="white")
+sleep(5)
 
 def main():
 	controller()
 
 def controller():
-	with canvas(device, dither=True) as draw:
-		draw.rectangle((10, 10, 30, 30), outline="white", fill="red")
 	button.when_pressed = switchMode
 	rotor.when_rotated = setBrightness
 	pause()
@@ -56,7 +59,7 @@ def setBrightness():
 	try:
 		amount = 6000 * steps
 		if colour is None:
-			colour = list(light.get_color())
+			colour = list(lightDevice.get_color())
 		brightness = colour[2]
 		brightness += amount
 		if brightness < 0:
@@ -65,7 +68,7 @@ def setBrightness():
 			brightness = 65535
 		colour[2] = brightness
 		print(str(round((brightness / 65535) * 100)) + "%")
-		light.set_color(colour, 500, True)
+		lightDevice.set_color(colour, 500, True)
 	except Exception as error:
 		print("An exception occurred:", error)
 
@@ -76,7 +79,7 @@ def setColour():
 	try:
 		amount = 2000 * steps
 		if colour is None:
-			colour = list(light.get_color())
+			colour = list(lightDevice.get_color())
 		hue = colour[0]
 		hue += amount
 		if hue < 0:
@@ -85,7 +88,7 @@ def setColour():
 			hue -= 65535
 		colour[0] = hue
 		print(str(round((hue / 65535) * 360)) + "°")
-		light.set_color(colour, 500, True)
+		lightDevice.set_color(colour, 500, True)
 	except Exception as error:
 		print("An exception occurred:", error)
 
