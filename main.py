bbe2e70954e69573accd5c39f2cf10a5c1dca6bd
Cooliew <cooliew101@gmail.com>
2023-09-19 13:49:08 +1000

    Added display interface init code

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def setColour():
 			hue -= 65535
 		colour[0] = hue
 		with canvas(displayDevice) as draw:
-			hslColour = ImageColor.getrgb(hsl(round((hue / 65535)) * 360) * 100), 100, 50))
+			hslColour = ImageColor.getrgb(hsl((round((hue / 65535)) * 360) * 100), 100, 50)
 			draw.rectangle(displayDevice.bounding_box, fill="black")
 			draw.rounded_rectangle(displayDevice.bounding_box, radius=10, fill=hslColour,outline="white", width=2)
 		lightDevice.set_color(colour, 500, True)
